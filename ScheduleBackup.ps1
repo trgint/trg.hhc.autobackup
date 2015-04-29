aeeeb2a0d2c8bd360d6f53d7c57d8bed2ba9a85d
Vincent De Smet <vincent.drl@gmail.com>
2015-04-29 21:02:39 +0700

    script creation of scheduled task

---
+++ ScheduleBackup.ps1
@@ -0,0 +1,18 @@
+﻿$xmlfile = "TaskConfig.xml"
+$url = "https://bitbucket.org/trginternational/trg.hhc.autobackup/raw/master/$xmlfile"
+
+Write-Host -ForegroundColor Green "Downloading TaskConfig xml template"
+if ($PSVersionTable.PSVersion -ge (new-object 'Version' 3,0))
+{
+    #Powershell version 3 and above support Invoke-WebRequest
+    Invoke-WebRequest ("https://bitbucket.org/trginternational/trg.hhc.autobackup/raw/master/$xmlfile") -OutFile $xmlfile
+} else {
+    (New-Object System.IO.StreamReader((New-Object System.Net.WebClient).OpenRead($url))).ReadToEnd() | Out-File $xmlfile 
+}
+pause
+
+Write-Host -ForegroundColor Green "Please provide credentials under which the backup task should run"
+$creds = Get-Credential
+
+#refer to https://msdn.microsoft.com/en-us/library/windows/desktop/bb736357(v=vs.85).aspx
+schtasks /Create /XML $xmlfile /RU $creds.UserName /RP $creds.GetNetworkCredential().Password /TN "SunSystems Auto backup"  
